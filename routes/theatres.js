a91fd9de85c98ae9cf3b2c408c26aef7f9953c6e
MANDO <starkmsh@gmail.com>
2024-06-15 02:22:33 +0530

    methods for addMovie , addTheatres

--- routes/theatres.js
+++ routes/theatres.js
@@ -1,9 +1,10 @@
 import expresss from 'express';
 import {connection} from '../index.js';
-import {getTheatres} from '../controllers/theatre.js';
+import {getTheatres,addTheatre} from '../controllers/theatre.js';
 
 const router = expresss.Router();
 
 router.get("/", getTheatres);
+router.post("/", addTheatre)
 
 export default router;
