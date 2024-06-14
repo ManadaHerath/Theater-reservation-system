b547303f0f0025e665e1c8e91ce7278adb153b70
MANDO <starkmsh@gmail.com>
2024-06-15 02:34:03 +0530

    Gave admin access to addMovies + addTheatres
    similar methods can be implemented later sameway.

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,11 @@
 import expresss from 'express';
 import {connection} from '../index.js';
 import {getMovies,addMovies} from '../controllers/movie.js';
+import { verifyToken, verifyUser,verifyAdmin} from '../util/verify_token.js';
 
 const router = expresss.Router();
 
 router.get("/", getMovies);
-router.post("/", addMovies);
+router.post("/",verifyAdmin ,addMovies);
 
 export default router;
--- routes/theatres.js
+++ routes/theatres.js
@@ -1,10 +1,11 @@
 import expresss from 'express';
 import {connection} from '../index.js';
 import {getTheatres,addTheatre} from '../controllers/theatre.js';
+import { verifyToken, verifyUser,verifyAdmin} from '../util/verify_token.js';
 
 const router = expresss.Router();
 
 router.get("/", getTheatres);
-router.post("/", addTheatre)
+router.post("/",verifyAdmin , addTheatre)
 
 export default router;
