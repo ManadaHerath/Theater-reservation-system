a57bdd759d93d1d2896b1daffbe1c11aeceb3edd
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-06-15 20:41:58 +0530

    change start scipt into nodemon

--- routes/theatre_Show_Times.js
+++ routes/theatre_Show_Times.js
@@ -1,19 +1,21 @@
-import expresss from 'express';
-import {connection} from '../index.js';
+import expresss from "express";
+import { connection } from "../index.js";
 
 const router = expresss.Router();
+const dbquery =
+  "SELECT start_time,end_time,title FROM show_times INNER JOIN movies ON show_times.movie_id = movies.id where show_times.theatre_id = ?";
 
 //when gives theatre_id as query parameter, it returns the movie title and showtimes of that theatre
 router.get("/", async (req, res) => {
-    try {
-        const { theatre_id } = req.query;
-      const [showTimes] = await connection.query('SELECT start_time,end_time,title FROM show_times INNER JOIN movies ON show_times.movie_id = movies.id where show_times.theatre_id = ?',[theatre_id]);
+  try {
+    const { theatre_id } = req.query;
+    const [showTimes] = await connection.query(dbquery, [theatre_id]);
 
-      res.json(showTimes);
-    } catch (error) {
-      console.error('Error fetching showtimes:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+    res.json(showTimes);
+  } catch (error) {
+    console.error("Error fetching showtimes:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 export default router;
