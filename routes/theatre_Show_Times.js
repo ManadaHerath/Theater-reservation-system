ae85b71fad8852a87fda28b68d172b485b97e2ba
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-06-15 20:53:48 +0530

    theatre_show_times api code changed

--- routes/theatre_Show_Times.js
+++ routes/theatre_Show_Times.js
@@ -5,8 +5,7 @@ import {getShowTimes} from '../controllers/theatre_show_times.js';
 
 
 const router = expresss.Router();
-const dbquery =
-  "SELECT start_time,end_time,title FROM show_times INNER JOIN movies ON show_times.movie_id = movies.id where show_times.theatre_id = ?";
+
 
 
 router.get("/", getShowTimes);
