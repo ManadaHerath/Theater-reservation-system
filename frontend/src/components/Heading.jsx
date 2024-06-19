3d83afe21a846322e450d6a8fa4eb163368abc08
Pramod Hasaranga <pramodgamage7846@gmail.com>
2024-06-20 00:35:49 +0530

    Create Navbar

--- frontend/src/components/Heading.jsx
+++ frontend/src/components/Heading.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Heading = () => {
   return (
-    <div className="bg-indigo-600 py-4 shadow-md mb-8">
+    <div className="py-4 bg-indigo-600 shadow-md ">
       <h1 className="text-4xl font-bold text-center text-white">Book Your Seats Now!</h1>
-      <p className="text-lg text-center text-gray-200 mt-2">Reserve tickets for your favorite shows</p>
+      <p className="mt-2 text-lg text-center text-gray-200">Reserve tickets for your favorite shows</p>
     </div>
   );
 }
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import search_logo from '../assets/search-w.png'
 
-
-const NavBar = ()=>{
-    return(
-        <div>
-            <h1>NavBar</h1>
-        </div>
-    )
-}
+const NavBar = () => {
+  return (
+    <div className="flex items-center justify-between h-16 p-4 text-white bg-gray-800">
+      <img src="" alt="logo" className="w-40 cursor-pointer" />
+      <ul className="flex space-x-4 text-lg">
+        <li>
+          <Link to="/home" className="hover:text-gray-400">Home</Link>
+        </li>
+        <li>
+          <Link to="/movies" className="hover:text-gray-400">Movies</Link>
+        </li>
+        <li>
+          <Link to="/schedule" className="hover:text-gray-400">Schedule</Link>
+        </li>
+        <li>
+          <Link to="/theatres" className="hover:text-gray-400">Theatres</Link>
+        </li>
+      </ul>
+      <div className="flex items-center p-2 space-x-2 bg-gray-700 rounded-full">
+        <input
+          type="text"
+          placeholder="Search"
+          className="p-1 text-lg text-white placeholder-gray-400 bg-transparent border-0 outline-none"
+        />
+        <img src={search_logo} alt="search" className="w-5 cursor-pointer" />
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
