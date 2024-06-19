26b4888939a1e48af1ceb097fa2902d62b3255ad
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-06-19 22:45:07 +0530

    Register-form component added without google account loging

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,44 @@
 import React from "react";
+import ReactDOM from "react-dom";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import MovieList from "./components/Movies";
 import Heading from "./components/Heading";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import TheatreList from "./components/Theatres";
-import MovieShedule from "./components/Shedule";
+import RegisterForm from "./components/User Registration/Register";
 
 const App = () => {
-  
-  return(
-    <div>
-      <Heading />
-      <NavBar />
-      <MovieList />
-      <TheatreList/>
-      <MovieShedule/>
-      <Footer />
-
+  return (
+    <div className="bg-black">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <MovieList />
+                <TheatreList />
+              </div>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <div>
+                <RegisterForm />
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
-
-}
+  );
+};
 
 export default App;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
