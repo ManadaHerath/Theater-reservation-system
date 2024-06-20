31316f9837a46f23058c3b2c26e020f2c0d23201
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-06-20 09:21:21 +0530

    App.js fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,10 @@
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
-
 import Home from "./pages/Home";
 import Movies from "./pages/MoviePage";
 import Theatres from "./pages/TheatrePage";
 import Schedule from "./pages/SchedulePage";
-
-
 import React from "react";
 import ReactDOM from "react-dom";
-
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import MovieList from "./components/Movies";
 import Heading from "./components/Heading";
 import NavBar from "./components/NavBar";
