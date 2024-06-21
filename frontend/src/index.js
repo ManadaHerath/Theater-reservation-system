e61d2e113e94ab9c6620a4d0255b9d4420d3f7f2
MANDO <starkmsh@gmail.com>
2024-06-21 23:07:38 +0530

    Created checkout-session using Stipe (Test mode).
    Has to fix bugs and complete the returning to successful / payment faliure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,9 @@ import App from './App';
 import './index.css';
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+
+//     <App />
+
+// );
