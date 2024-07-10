0f1c95b02ea96b7103cd788f1c13af242f3a6372
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-07-10 12:36:47 +0530

    footer small change added

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,11 +5,11 @@ const Footer = () => {
   useEffect(() => {
     const handleScroll = () => {
       const footer = document.getElementById('footer');
-      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 1) {
         footer.classList.remove('hidden');
-        footer.classList.add('block');
+        footer.classList.add('flex');
       } else {
-        footer.classList.remove('block');
+        footer.classList.remove('flex');
         footer.classList.add('hidden');
       }
     };
@@ -24,7 +24,7 @@ const Footer = () => {
 
   return (
     <div id="footer" className="hidden fixed inset-x-0 bottom-0 w-full py-4 bg-indigo-600 shadow-md">
-      <ul className="flex justify-center space-x-4 text-lg">
+      <ul className="flex space-x-16 text-lg mx-auto">
         <li>
           <Link to="/help" className="text-gray-200">Help & FAQ</Link>
         </li>
