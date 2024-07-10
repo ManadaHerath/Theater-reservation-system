5a311e38ad5a676ce651dd19982ded38f1a94886
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-07-10 18:18:12 +0530

    minor bug fixed in controller/theatre.js

--- backend/controllers/theatre.js
+++ backend/controllers/theatre.js
@@ -40,13 +40,8 @@ export const addTheatre = async (req, res, next) => {
         no_of_rows,
         no_of_columns,
         image_url
-      } = req.body;
-  
-      const [result] = await connection.query(
-        'INSERT INTO theatres (name, address, location, mobile_number, email, details, is_active, no_of_seats, no_of_rows, no_of_columns,image_url) VALUES (?, ?, ST_GeomFromText(?), ?, ?, ?, ?, ?, ?, ?,?)',
-        [name, address, `POINT(${location.lat} ${location.lng})`, mobile_number, email, details, is_active, no_of_seates, no_of_rows, no_of_columns,image_url]
-      );
-
+      ]
+    );
 
       const theatreIdQuery = 'SELECT id FROM theatres WHERE name = ? AND address = ? ORDER BY id DESC LIMIT 1';
       const [rows] = await connection.query(theatreIdQuery, [name, address]);
