ebfa650b69c119a4c460fb4f08a2a4fe4d0aea70
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-08-26 20:45:28 +0530

    schedule completed

--- backend/controllers/show_times.js
+++ backend/controllers/show_times.js
@@ -2,7 +2,7 @@ import {connection} from '../index.js';
 export const getAllShowTimes = async (req, res) => {
   try {
 
-    const [show_times] = await connection.query('SELECT start_time,end_time,m.title,m.poster_url,t.name FROM show_times inner join movies m on show_times.movie_id = m.id inner join theatres t on show_times.theatre_id = t.id');
+    const [show_times] = await connection.query('SELECT s.theatre_id,s.id,s.start_time,s.end_time,m.title,m.poster_url,t.name FROM show_times s inner join movies m on s.movie_id = m.id inner join theatres t on s.theatre_id = t.id');
     console.log(show_times);
     res.json(show_times);
   } catch (error) {
