792a84c9831698bc9b02817e6988baaf2a65b3e0
MANDO <starkmsh@gmail.com>
2024-09-29 17:34:28 +0530

    theatre-admin-showtimes
    showtimes adder component created

---
+++ frontend/src/components/Theatre Admin/ShowTimes Management/ShowTimes.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const ShowTimes = () => {
+    return (
+        <div>
+            Yo Theatre Admin Showtime here
+        </div>
+    )
+}
+
+export default ShowTimes;
+
