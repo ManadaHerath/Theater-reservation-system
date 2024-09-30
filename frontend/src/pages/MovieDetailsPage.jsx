73035787a9be5d5bab57031b0f6fac0de3dd82ea
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-09-30 15:44:25 +0530

    some bugs fixed

--- frontend/src/pages/MovieDetailsPage.jsx
+++ frontend/src/pages/MovieDetailsPage.jsx
@@ -1,7 +1,6 @@
 import MovieDetails from "../components/Movie/MovieDetails";
 import NavBar from "../components/NavBar/NavBar";
 
-
 export default function MovieDetailsPage() {
   return (
     <div>
