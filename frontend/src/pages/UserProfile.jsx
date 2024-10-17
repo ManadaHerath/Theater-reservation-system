988a6d219ed973b8243c8608340bc62dc9adca82
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-10-18 00:10:37 +0530

    password change option created

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -1,10 +1,12 @@
 import NavBar from "../components/NavBar/NavBar";
 import Profile from "../components/User Profile/Profile";
+import ChangePassword from "../components/User Profile/ChangePassword";
 
 const UserProfile = () => (
   <div>
     <NavBar />
     <Profile />
+    <ChangePassword />
   </div>
 );
 
