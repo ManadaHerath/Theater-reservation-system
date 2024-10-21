27345bb89ca337b7aa9866ffc865f66e6dd29728
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-10-21 14:13:24 +0530

    added chatbot more responsiveness

--- frontend/src/components/Chatbot/ChatBot.jsx
+++ frontend/src/components/Chatbot/ChatBot.jsx
@@ -19,7 +19,7 @@ const ChatBot = ({ chatbotId }) => {
       {/* Glowing and Floating Button */}
       <button
         onClick={handleChatBubbleClick}
-        className="relative w-12 h-8 sm:w-24 sm:h-12 text-base sm:text-lg hover:text-white bg-white text-black font-bold rounded-2xl hover:bg-blue-800 transition duration-300 flex justify-center items-center shadow-xl animate-bounce-slow"
+        className="relative w-[70px] h-12 sm:w-24 sm:h-12 text-base sm:text-lg hover:text-white bg-white text-black font-bold rounded-2xl hover:bg-blue-800 transition duration-300 flex justify-center items-center shadow-xl animate-bounce-slow"
         style={{
           boxShadow: "0 0 30px rgba(0, 150, 255, 0.7)", // Glowing effect
           //backgroundImage: "url(https://firebasestorage.googleapis.com/v0/b/movie-mingle-2ec48.appspot.com/o/vecteezy_robot-chatbot-aesthetic_25271424.png?alt=media&token=1b54f33a-3f9a-451d-be88-16fb07a84521)", // Add logo if needed
